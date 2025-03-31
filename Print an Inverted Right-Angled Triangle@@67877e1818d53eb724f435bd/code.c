a3e979c60ad2f5588d2bbbed97cb41a895e8cd90
harshitsharma0385 <harshit1334.becse24@chitkara.edu.in>
2025-03-31 22:12:43 +0530

    Solution to Print an Inverted Right-Angled Triangle

--- Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
+++ Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
@@ -1,10 +1,22 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int n, i, j;
-    for(i = n; i<n+1; i--){
-        for(j = 0; j<n+1; j++){
-            printf("*");
+    
+    // Ask the user for the number of rows
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    
+    // Loop from 'n' down to 1
+    for (i = n; i >= 1; i--) {
+        // Print 'i' stars in each row
+        for (j = 1; j <= i; j++) {
+            printf("* ");
         }
+        // Move to the next line after each row
+        printf("\n");
     }
+    
     return 0;
 }
+
